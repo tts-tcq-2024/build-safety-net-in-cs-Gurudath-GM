c70c7d7d2d4e31a1330dd81e763d8651e10f7a50
Gurudath G M <princegguruk@gmail.com>
2024-07-04 19:48:48 +0530

    Update Soundex.Tests.cs
    Added methods to test the code

--- Soundex.Tests.cs
+++ Soundex.Tests.cs
@@ -11,8 +11,48 @@ public class SoundexTests
     [Fact]
     public void HandlesSingleCharacter()
     {
-        Assert.Equal("A000", Soundex.GenerateSoundex("A"));
+        Assert.Equal(Soundex.GenerateSoundex("E"), "E000");
     }
 
-   
+    [Fact]
+    public void HandlesMultipleCharacter()
+    {    
+        Assert.Equal(Soundex.GenerateSoundex("Smith"), "S530");
+    }
+
+    [Fact]
+    public void HandlesNameWithOnlyVowels()
+    {
+        Assert.Equal(Soundex.GenerateSoundex("Ouea"), "O000");
+    }
+
+    [Fact]
+    public void HandlesNameWithOnlyConsonants()
+    {
+        Assert.Equal(Soundex.GenerateSoundex("Cry"), "C600");
+    }
+
+    [Fact]
+    public void HandlesUpperAndLowerCaseLetters()
+    {
+        Assert.Equal(Soundex.GenerateSoundex("sMiTh"), "S530");
+    }
+
+    [Fact]
+    public void HandlesNamesWithDuplicateConsonants()
+    {
+        Assert.Equal(Soundex.GenerateSoundex("Tennessee"), "T520");
+    }
+
+    [Fact]
+    public void HandlesNamesWithNonAlphabeticCharacters()
+    {
+        Assert.Equal(Soundex.GenerateSoundex("ONeill"), "O540");
+    }
+
+    [Fact]
+    public void HandlesLongNames()
+    {
+        Assert.Equal(Soundex.GenerateSoundex("Washington"), "W252");
+    }
 }
