f146a3ed4a47c8757e78dcdef0eb9f9b845a0a36
Gurudath G M <princegguruk@gmail.com>
2024-08-20 14:09:09 +0530

    Update Soundex.cs

--- Soundex.cs
+++ Soundex.cs
@@ -4,52 +4,6 @@ using System.Linq;
 
 public class Soundex
 {
-    private static void SoundexCodeAppend(StringBuilder soundex, char value)
-    {
-        soundex.Append(char.ToUpper(value));
-    }
-
-    private static void GetSoundexCodeProcess(StringBuilder soundex, string name)
-    {
-        char prevCode = GetSoundexCode(name[0]);
-
-        foreach (var c in name.Skip(1))
-        {
-            if (soundex.Length >= 4) break;
-
-            AppendValidCode(soundex, c, ref prevCode);
-        }
-    }
-
-    private static void AppendValidCode(StringBuilder soundex, char c, ref char prevCode)
-    {
-        char code = GetSoundexCode(c);
-        if (CheckForValidCode(code, prevCode))
-        {
-            soundex.Append(code);
-            prevCode = code;
-        }
-    }
-
-    private static bool CheckForValidCode(char code, char prevCode)
-    {
-        return code != '0' && code != prevCode;
-    }
-
-    private static void CheckSoundex(StringBuilder soundex)
-    {
-        while (soundex.Length < 4)
-        {
-            soundex.Append('0');
-        }
-    }
-    
-    private static char GetSoundexCode(char c)
-    {
-        c = char.ToUpper(c);
-        return SoundexConstants.CodeMap.TryGetValue(c, out var code) ? code : '0';
-    }
-    
     public static string GenerateSoundex(string name)
     {
         if (string.IsNullOrEmpty(name))
@@ -58,9 +12,9 @@ public class Soundex
         }
 
         StringBuilder soundex = new StringBuilder();
-        SoundexCodeAppend(soundex, name[0]);
-        GetSoundexCodeProcess(soundex, name);
-        CheckSoundex(soundex);
+        Util.AppendInitialCharacter(soundex, name[0]);
+        Util.ProcessName(soundex, name);
+        Util.FinalizeSoundex(soundex);
         return soundex.ToString();
     }
 }
